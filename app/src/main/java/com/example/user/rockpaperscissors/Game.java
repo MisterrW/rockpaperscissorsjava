92337dccf1c144bf4e7713f663e9538a95faa11a
William Robertson <w.robertson305@gmail.com>
2016-12-14 14:46:49 +0000

    layout tweaks

---
+++ app/src/main/java/com/example/user/rockpaperscissors/Game.java
@@ -0,0 +1,98 @@
+package com.example.user.rockpaperscissors;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class Game extends AppCompatActivity {
+
+    TextView exampleText;
+    Button rockButton;
+    Button paperButton;
+    Button scissorsButton;
+    Button spockButtton;
+    Button lizardButton;
+    RockPaperScissors choice;
+    TextView answerText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("RockPaperScissors", "onCreateCalled");
+        super.onCreate(savedInstanceState);
+
+        if (getResources().getConfiguration().orientation ==
+                Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.pt_activity_main);
+        } else {
+            setContentView(R.layout.ls_activity_main);
+        }
+
+        exampleText=(TextView)findViewById(R.id.example_text);
+        rockButton=(Button)findViewById(R.id.rock_button);
+        paperButton=(Button)findViewById(R.id.paper_button);
+        scissorsButton=(Button)findViewById(R.id.scissors_button);
+        spockButtton=(Button)findViewById(R.id.spock_button);
+        lizardButton=(Button)findViewById(R.id.lizard_button);
+
+        rockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                choice = new RockPaperScissors("rock");
+                String result = choice.winCheck();
+//                Log.d("RockPaperScissors", result);
+                answerText = (TextView)findViewById(R.id.answer_text);
+                answerText.setText(result);
+            }
+        });
+
+        paperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                choice = new RockPaperScissors("paper");
+                String result = choice.winCheck();
+//                Log.d("RockPaperScissors", result);
+                answerText = (TextView)findViewById(R.id.answer_text);
+                answerText.setText(result);
+            }
+        });
+
+        scissorsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                choice = new RockPaperScissors("scissors");
+                String result = choice.winCheck();
+//                Log.d("RockPaperScissors", result);
+                answerText = (TextView)findViewById(R.id.answer_text);
+                answerText.setText(result);
+            }
+        });
+
+        spockButtton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                choice = new RockPaperScissors("spock");
+                String result = choice.winCheck();
+//                Log.d("RockPaperScissors", result);
+                answerText = (TextView)findViewById(R.id.answer_text);
+                answerText.setText(result);
+            }
+        });
+
+        lizardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                choice = new RockPaperScissors("lizard");
+                String result = choice.winCheck();
+//                Log.d("RockPaperScissors", result);
+                answerText = (TextView)findViewById(R.id.answer_text);
+                answerText.setText(result);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/user/rockpaperscissors/RockPaperScissors.java
@@ -0,0 +1,94 @@
+package com.example.user.rockpaperscissors;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RockPaperScissors {
+    String playerChoice;
+    String computerChoice;
+    ArrayList<String> choices;
+
+    public RockPaperScissors(String choice) {
+        choices = new ArrayList<>();
+        setUpChoices();
+        playerChoice = choice.toLowerCase();
+        computerChoice = getComputerChoices().toLowerCase();
+    }
+
+    public RockPaperScissors(String computerChoice, String playerChoice) {
+        this.computerChoice = computerChoice.toLowerCase();
+        this.playerChoice = playerChoice.toLowerCase();
+    }
+
+    public String getPlayerChoice() {
+        return playerChoice;
+    }
+
+    public int getChoicesCount() {
+        return choices.size();
+    }
+
+    public String getChoiceAtIndex(int index) {
+        return choices.get(index);
+    }
+
+    public String getComputerChoices() {
+        Random rand = new Random();
+        int listSize = getChoicesCount();
+        int index = rand.nextInt(listSize);
+        String choice = getChoiceAtIndex(index);
+        return choice;
+    }
+
+    public void setUpChoices() {
+        String[] choicesToAdd = {
+                "rock",
+                "paper",
+                "scissors",
+                "lizard",
+                "spock"
+        };
+        for (String choice : choicesToAdd) {
+            choices.add(choice);
+        }
+    }
+
+    public String winCheck() {
+        if (playerChoice.equals("rock") && computerChoice.equals("scissors") || (playerChoice
+                .equals("scissors") && computerChoice.equals("rock") || (playerChoice
+                .equals("rock") && computerChoice.equals("lizard") || (playerChoice
+                .equals("lizard") && computerChoice.equals("rock"))))) {
+            return "Player chose " + playerChoice + ", computer chose " + computerChoice + ". " +
+                    "Rock wins!";
+        }
+        else if (playerChoice.equals("paper") && computerChoice.equals("rock") || (playerChoice
+                .equals("rock") && computerChoice.equals("paper") || (playerChoice.equals
+                ("paper") && computerChoice.equals("spock") || (playerChoice.equals("spock") &&
+                computerChoice.equals("paper"))))) {
+            return "Player chose " + playerChoice + ", computer chose " + computerChoice + ". " +
+                    "Paper wins!";
+        }
+        else if (playerChoice.equals("paper") && computerChoice.equals("scissors")
+                || (playerChoice.equals("scissors") && computerChoice.equals("paper") ||
+                (playerChoice.equals("scissors") && computerChoice.equals("lizard") ||
+                        (playerChoice.equals("lizard") && computerChoice.equals("scissors"))))) {
+            return "Player chose " + playerChoice + ", computer chose " + computerChoice + ". " +
+                    "Scissors wins!";
+        }
+        else if (playerChoice.equals("spock") && computerChoice.equals("rock")
+                    || (playerChoice.equals("rock") && computerChoice.equals("spock") ||
+                    (playerChoice.equals("spock") && computerChoice.equals("scissors") ||
+                            (playerChoice.equals("scissors") && computerChoice.equals("spock"))))) {
+            return "Player chose " + playerChoice + ", computer chose " + computerChoice + ". Spock wins!";
+        }
+        else if (playerChoice.equals("lizard") && computerChoice.equals("paper")
+                || (playerChoice.equals("paper") && computerChoice.equals("lizard") ||
+                (playerChoice.equals("lizard") && computerChoice.equals("spock") ||
+                        (playerChoice.equals("spock") && computerChoice.equals("lizard"))))) {
+            return "Player chose " + playerChoice + ", computer chose " + computerChoice + ". Lizard wins!";
+        }
+
+        return "Player chose " + playerChoice + ", computer chose " + computerChoice + ". Draw.";
+    }
+
+}
